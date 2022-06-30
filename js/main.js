5a11847388a53b10711c29a08de8e3f8f24e1696
Tomás Moschen <moschen.tomas@gmail.com>
2022-06-29 23:44:31 -0300

    1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Nombre")
+for (let i = 1; i <= 5; i++) {
+    let ingresarTituloLibro = prompt("Título del libro");
+    alert("Felicitaciones "+nombre + ", terminaste la lecutura de " + ingresarTituloLibro + ". Este es tu libro N° " + i +"/5" +".");
+}
